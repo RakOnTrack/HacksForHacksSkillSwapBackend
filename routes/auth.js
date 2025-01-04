f5d41293d60f8661a05bf82c363b4d9e8a0a3529
RakOnTrack <eric_rak@hotmail.com>
2025-01-03 22:33:39 -0500

    simple auth

---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// Register route
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // Check if username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
+    }
+
+    // Create new user
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Error registering user", error });
+  }
+});
+
+// Login route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(400).json({ message: "Error logging in", error });
+  }
+});
+
+
+module.exports = router;
